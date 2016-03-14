70771ae7409e027526339739d9844e0159fdae37
Eliott <eliott.benoit4@gmail.com>
2016-03-14 22:02:59 +0000

    cleaning

--- script/projet.js
+++ script/projet.js
@@ -5,10 +5,3 @@ function next(i) {
 		tmp = 1;
 	$('#img' + tmp).css('z-index', 1).siblings().css('z-index', -1);
 }
-
-$(document).keydown(function(e){
-	switch (e.which){
-		case 37: window.location.href = "projet_standjpo.html"; break; //lien du projet d'avant
-		case 39: window.location.href = "projet_germinal.html"; break; // lien du projet d'après
-	}
-});
