2fdf5a979ff72d7f5b7cea71b3ff748d6fe850b8
Pricilla Degardin <pricilladegardin@pc23.home>
2016-09-04 19:42:52 +0200

    add loading gif and gallery

--- script/projet.js
+++ script/projet.js
@@ -1,7 +1,17 @@
+$("#loading").focus();
+
 var tmp = 1;
 function next(i) {
 	tmp++;
 	if(tmp == i+1)
 		tmp = 1;
 	$('#img' + tmp).css('z-index', 1).siblings().css('z-index', -1);
+}
+
+// une fois la page chargée, retire le gif de loading et affiche les images
+window.onload = function() {
+	setTimeout(function() {
+		$('#loading').hide();
+		$('#images').css('visibility', 'visible');
+	}, 1);
 }
